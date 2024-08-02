195b829db419a053739bf777d5448be05e1a26e9
Javier Salas <javier.salas.m@usach.cl>
2024-08-02 11:05:30 -0400

    Se agregan notaciones Lombok a Vehiculo

--- ms1/src/main/java/com/tingeso/ms1/entities/VehiculoEntity.java
+++ ms1/src/main/java/com/tingeso/ms1/entities/VehiculoEntity.java
@@ -1,8 +1,17 @@
 package com.tingeso.ms1.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "vehiculo")
 public class VehiculoEntity {
     @Id
